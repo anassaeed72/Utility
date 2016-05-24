2d1ae9011c7686291c81bdc98d9957592bd087a0
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 21:52:04 -0400

    Test/

---
+++ Test/Test/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Anas Saeed on 30/01/2015.
+//  Copyright (c) 2015 Anas Saeed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << false;
+    return 0;
+}
+
