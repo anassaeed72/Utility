b365528db01f9380967c1dc39358fac1145e011d
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 21:50:32 -0400

    Printer/

---
+++ Printer/Printer/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  Printer
+//
+//  Created by Anas Saeed on 18/08/2014.
+//  Copyright (c) 2014 Anas Saeed. All rights reserved.
+//
+
+#include <iostream>
+//#include <OP>
+
+//void printImage(std::string image)
+//{
+//    QPrinter printer;
+//    QPrintDialog printDialog(&printer, this);
+//    if (printDialog.exec()) {
+//        QPainter painter(&printer);
+//        QRect rect = painter.viewport();
+//        QSize size = image.size();
+//        size.scale(rect.size(), Qt::KeepAspectRatio);
+//        painter.setViewport(rect.x(), rect.y(),
+//                            size.width(), size.height());
+//        painter.setWindow(image.rect());
+//        painter.drawImage(0, 0, image);
+//    }
+//}
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
