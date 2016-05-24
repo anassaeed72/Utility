917d40f7706224ac2295b347eb9de72123347b66
Anas Saeed <anassaeed@Anass-MacBook-Pro.local>
2016-05-23 21:52:13 -0400

    Line/

---
+++ Line/Line/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Line
+//
+//  Created by Anas Saeed on 10/24/14.
+//  Copyright (c) 2014 Anas Saeed. All rights reserved.
+//
+
+#include <iostream>
+#define _LINE_ = 5
+int main(int argc, const char * argv[])
+{
+
+//    std::cout << _LINE_ + 1
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    printf("The value of Line is ");
+    return 0;
+}
+
